bcce3377e9f583d331470b217950c93be58ab652
MamutoKing <cameron.churchillmedina@csuglobal.edu>
2026-05-18 01:01:43 +0000

    first commit

---
+++ weekly_temperatures.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class weekly_tempuratures { 
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        //Declare Arrays and index
+        String weekDay[] = {"Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int tempAVG[] = {79, 81, 77, 83, 85, 83, 86};
+        String input;
+        int i;
+
+        //Propt user to enter Average temperatures
+        System.out.println("Enter \"week\" to see average teperatures for each day this week.");
+        input = scnr.nextLine();
+
+        if (input.equalsIgnoreCase("week")) {
+            for (i = 0; i < weekDay.length; ++i) {
+                System.out.println(weekDay[i] + ": " + tempAVG[i] + "°F");
+            }
+        }
+        else { 
+            System.out.println("Invalid input.");
+        }
+        Scanner.close();
+    }
+
+}
